d98ed9b778ee132483b2f5988f3679b0c60c34bb
chnt <cihantklc@gmail.com>
2022-06-27 00:06:39 +0200

    - readme updated - json server added - shared styled components added

---
+++ src/shared/styled/box.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+const StyledBox = styled(Box)(() => ({
+  display: 'flex',
+  position: 'relative'
+}));
+
+export default StyledBox;
